f23df377493b1c90b64d7e17f5cb103fe4bdec52
Henrilja <loghenrik@gmail.com>
2024-03-11 13:40:32 +0100

    get patjh

--- helpers.py
+++ helpers.py
@@ -1,9 +1,12 @@
 import os
 
 def read_file(path):
-    file_path = os.path.join("/datasets/tdt4265/ad/NAPLab-LiDAR", path)
+    file_path = get_path(path)
 
     with open(file_path, "r") as file:
         file_contents = file.read()
 
     return file_contents
+
+def get_path(path):
+    return os.path.join("/datasets/tdt4265/ad/NAPLab-LiDAR", path)
