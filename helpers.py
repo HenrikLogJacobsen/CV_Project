c38c1c7406e6cf84777611ed322fd521f5221ce5
Henrilja <loghenrik@gmail.com>
2024-03-11 14:46:52 +0100

    box visualization for image 1

--- helpers.py
+++ helpers.py
@@ -1,12 +1,7 @@
 import os
 
 def read_file(path):
-    file_path = get_path(path)
-
-    with open(file_path, "r") as file:
+    with open(path, "r") as file:
         file_contents = file.read()
 
     return file_contents
-
-def get_path(path):
-    return os.path.join("/datasets/tdt4265/ad/NAPLab-LiDAR", path)
