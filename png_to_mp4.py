76e0da7fa81df612642c1872ed469a3de934dfe1
Henrilja <loghenrik@gmail.com>
2024-03-14 16:04:43 +0100

    ram errors

--- png_to_mp4.py
+++ png_to_mp4.py
@@ -1,8 +1,9 @@
 import cv2
 import os
 
-image_sample = cv2.imread('data/images/frame_000000.PNG')
-image_height, image_width, _ = image_sample.shape
+# image_sample = cv2.imread('data/images/frame_000000.PNG')
+# image_height, image_width, _ = image_sample.shape
+image_shape = (128,1024)
 
 # Get the list of image files in the directory
 with open('data/train.txt', 'r') as file:
@@ -12,7 +13,7 @@ with open('data/train.txt', 'r') as file:
 video_file = 'output_video.mp4'
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
-video_writer = cv2.VideoWriter(video_file, fourcc, 30, (image_width, image_height))
+video_writer = cv2.VideoWriter(video_file, fourcc, 30, image_shape)
 
 # Iterate over the image files and write each frame to the video
 for image_file in image_files:
--- yolov8.py
+++ yolov8.py
@@ -3,34 +3,18 @@ import cv2
 import os
 
 
-
-# Load in a sample image
 image_shape = (128,1024)
-image_sample = cv2.imread('data/images/frame_000000.PNG')
-image_height, image_width, _ = image_sample.shape
-
-# Get the list of image files in the directory
-with open('data/train.txt', 'r') as file:
-    image_files = [line.strip() for line in file if line.strip().endswith('.PNG')]
-
-# Create a VideoWriter object
-video_file = 'output_video.mp4'
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-
-video_writer = cv2.VideoWriter(video_file, fourcc, 30, (image_width, image_height))
-
-# Iterate over the image files and write each frame to the video
-for image_file in image_files:
-    image_path = os.path.join('data/', image_file)
-    frame = cv2.imread(image_path)
-    video_writer.write(frame)
-
-# Release the VideoWriter object
-video_writer.release()
+source = 'output_video.mp4'
 
 # Pretrained YOLOv8n model
 model = YOLO('yolov8n.pt')
 
+# Setting detectable classes
+with open('data/names.txt', 'r') as f:
+    classes = [line.strip() for line in f]
+
+# Set the class filter
+
 # Inference
-results = model(source=video_file, conf=.4, imgsz=(image_height,image_width), save=True, save_txt=True)
+results = model(source=source, conf=.4, imgsz=image_shape, save=True, save_txt=True, classes=classes, stream=True)
 
