7290577efea0fa7dd81406c10f665ad7531b8f5e
Henrilja <loghenrik@gmail.com>
2024-03-14 15:27:57 +0100

    yolov8 model is a nice starting point

--- yolov8.py
+++ yolov8.py
@@ -1,7 +1,15 @@
 from ultralytics import YOLO
+import cv2
+
+
+
+# Load in a sample image
+image_sample = cv2.imread('data/images/frame_000000.PNG')
+image_height, image_width, _ = image_sample.shape
 
 # Pretrained YOLOv8n model
 model = YOLO('yolov8n.pt')
 
 # Inference
-results = model(source='test.mp4', show=True, save=True)
+results = model(source='data/images/', conf=.4, imgsz=(image_height,image_width), save=True, save_txt=True)
+
